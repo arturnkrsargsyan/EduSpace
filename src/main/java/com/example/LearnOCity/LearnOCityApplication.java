3d0be921326e1ec9bccb3c1546fd3706ae684f01
arturnkrsargsyan <arturnkr.sargsyan@gmail.com>
2023-08-08 10:45:51 +0400

    Inital commit

---
+++ src/main/java/com/example/LearnOCity/LearnOCityApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearnOCity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOCityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LearnOCity/LearnOCityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearnOCity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnOCityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
